0affc72bb9a41a6d26b6f73872085f78a1ceab33
yandaki <78733984+yandaki@users.noreply.github.com>
2022-07-05 16:22:16 +0200

    Update password_generator.py

--- password generator/password_generator.py
+++ password generator/password_generator.py
@@ -1,1 +1,17 @@
+import random
+import string
+
+
+def generateur_password(nbre_password, longueur):
+    caractere = string.ascii_letters + string.digits + string.punctuation
+    for i in range(nbre_password):
+        password = ''
+        for j in range(longueur):
+            password += random.choice(caractere)
+        print(password)
+
+
+nbre_password = int(input('Combien de mots de passe voulez-vous générer ? : '))
+longueur = int(input('Quelle est la longueur des mots de passe générés ? : '))
+generateur_password(nbre_password, longueur)
 
